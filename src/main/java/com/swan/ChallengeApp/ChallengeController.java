43ea937ec541279baafeac9b7d268fd52f52e985
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-01-04 19:20:56 +0700

    Final Update and Modification

--- src/main/java/com/swan/ChallengeApp/ChallengeController.java
+++ src/main/java/com/swan/ChallengeApp/ChallengeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/challenges")
 public class ChallengeController {
 
     private ChallengeService challengeService;
@@ -15,29 +16,7 @@ public class ChallengeController {
         this.challengeService = challengeService;
     }
 
-    @GetMapping("/challenges")
-    public ResponseEntity<List<Challenge>> getAllChallenges() {
-        return new ResponseEntity<>(challengeService.getAllChallenges(), HttpStatus.OK);
-    }
-
-    @GetMapping("/challenges/{month}")
-    public ResponseEntity<Challenge> getChallengeByMonth(@PathVariable String month) {
-        Challenge challenge = challengeService.getChallengeByMonth(month);
-        if (challenge != null)
-            return new ResponseEntity<>(challenge, HttpStatus.OK);
-        else
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-//    @GetMapping("/challenges/{id}")
-//    public Challenge getChallengeById(@PathVariable Long id) {
-//        Challenge challenge = challengeService.getChallengeById(id);
-//        if (challenge != null)
-//            return challenge;
-//        else
-//            return null;
-//    }
-
-    @PostMapping("/challenges")
+    @PostMapping
     public ResponseEntity<String> addChallenge(@RequestBody Challenge challenge) {
         boolean isChallengeAdded = challengeService.addChallenge(challenge);
         if (isChallengeAdded)
@@ -45,4 +24,45 @@ public class ChallengeController {
         else
             return new  ResponseEntity<>("Challenge could not be added", HttpStatus.BAD_REQUEST);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Challenge>> getAllChallenges() {
+        return new ResponseEntity<>(challengeService.getAllChallenges(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{month}")
+    public ResponseEntity<Challenge> getChallengeByMonth(@PathVariable String month) {
+        Challenge challenge = challengeService.getChallengeByMonth(month);
+        if (challenge != null)
+            return new ResponseEntity<>(challenge, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateChallenge(@PathVariable Long id, @RequestBody Challenge updatedChallenge) {
+        boolean isChallengeUpdated = challengeService.updateChallenge(id, updatedChallenge);
+        if (isChallengeUpdated)
+            return new ResponseEntity<>("Challenge updated successfully", HttpStatus.OK);
+        else
+            return new  ResponseEntity<>("Challenge could not be updated", HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteChallenge(@PathVariable Long id) {
+        boolean isChallengeDeleted = challengeService.deleteChallenge(id);
+        if (isChallengeDeleted)
+            return new ResponseEntity<>("Challenge deleted successfully", HttpStatus.OK);
+        else
+            return new  ResponseEntity<>("Challenge could not be deleted", HttpStatus.BAD_REQUEST);
+    }
+
+    //    @GetMapping("/{id}")
+//    public Challenge getChallengeById(@PathVariable Long id) {
+//        Challenge challenge = challengeService.getChallengeById(id);
+//        if (challenge != null)
+//            return challenge;
+//        else
+//            return null;
+//    }
 }
